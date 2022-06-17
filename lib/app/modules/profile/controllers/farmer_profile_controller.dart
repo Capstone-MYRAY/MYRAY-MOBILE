d040bf4ccd7a7c83f7a9548a1f43260b0d4ea5f2
Nguyen Phan Quynh Anh <anhnpqse140645@gmail.com>
2022-06-17 22:25:21 +0700

    add_view_farmer_profile

---
+++ lib/app/modules/profile/controllers/farmer_profile_controller.dart
@@ -0,0 +1,28 @@
+
+import 'package:get/get.dart';
+import 'package:myray_mobile/app/data/models/account.dart';
+import 'package:myray_mobile/app/modules/auth/controllers/auth_controller.dart';
+import 'package:myray_mobile/app/modules/profile/profile_repository.dart';
+import 'package:myray_mobile/app/shared/utils/auth_credentials.dart';
+
+class FarmerProfileController extends GetxController{
+
+  final _profileRepository = Get.find<ProfileRepository>();
+  var user = Account().obs;
+
+  @override
+  void onInit() async {
+    super.onInit();
+    await getUserInfor();
+
+  }
+
+  getUserInfor() async {
+    Account? user = await _profileRepository.getUser(AuthCredentials.instance.user!.id!);
+     if(user != null){
+      this.user.value = user;
+     }
+  }
+
+ 
+}
---
+++ lib/app/modules/profile/widgets/farmer_feature_option.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+import 'package:myray_mobile/app/modules/auth/controllers/auth_controller.dart';
+import 'package:myray_mobile/app/shared/constants/constants.dart';
+import 'package:myray_mobile/app/shared/icons/custom_icons_icons.dart';
+import 'package:myray_mobile/app/shared/widgets/feature_option.dart';
+
+class FarmerFeatureOptions extends StatelessWidget {
+  const FarmerFeatureOptions({Key? key}) : super(key: key);
+
+  _logout() {
+    final AuthController _authController = Get.find<AuthController>();
+    _authController.logOut();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        FeatureOption(
+          icon: CustomIcons.calendar_check,
+          title: AppStrings.titleCheckattendance,
+          subtitle: AppStrings.subtitleCheckattendance,
+          onTap: () {},
+        ),
+        FeatureOption(
+          icon: CustomIcons.work_history_outline,
+          title: AppStrings.titleHistoryJob,
+          subtitle: AppStrings.subtitleHistoryJob,
+          onTap: () {},
+        ),
+        FeatureOption(
+          icon: CustomIcons.account_heart_outline,
+          title: AppStrings.titleFavorite,
+          subtitle: AppStrings.subtitleFarmerFavorite,
+          onTap: () {},
+        ),
+        FeatureOption(
+          icon: CustomIcons.lock_outline,
+          title: AppStrings.titlePassword,
+          subtitle: AppStrings.subtitlePassword,
+          onTap: () {},
+        ),
+        FeatureOption(
+          icon: CustomIcons.logout,
+          title: AppStrings.titleLogout,
+          subtitle: AppStrings.subtitleLogout,
+          onTap: () => _logout(),
+        ),
+      ],
+    );
+  }
+}
