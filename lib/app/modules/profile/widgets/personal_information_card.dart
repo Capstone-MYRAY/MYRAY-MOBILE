c4f5a2d1b009885bdc0b1b072cb563c6fd909a5f
Ageha <agehachou2602@gmail.com>
2022-06-18 11:30:24 +0700

    fix text overflow on farmer profile details

--- lib/app/modules/profile/widgets/personal_information_card.dart
+++ lib/app/modules/profile/widgets/personal_information_card.dart
@@ -32,7 +32,15 @@ class PersonalInformation extends StatelessWidget {
               fontWeight: FontWeight.w300,
             ),
           ),
-          Text(right),
+          ConstrainedBox(
+            constraints: BoxConstraints(
+              maxWidth: (Get.width * 0.9 - 32 * 2) / 2,
+            ),
+            child: Text(
+              right,
+              softWrap: true,
+            ),
+          ),
         ],
       ),
     );
@@ -92,9 +100,17 @@ class PersonalInformation extends StatelessWidget {
                   const SizedBox(height: 16.0),
                   Row(
                     children: [
-                      user.value.aboutMe == null
-                          ? const Text('Chưa có thông tin')
-                          : Text(user.value.aboutMe!),
+                      Expanded(
+                        child: user.value.aboutMe == null
+                            ? const Text(
+                                'Chưa có thông tin',
+                                softWrap: true,
+                              )
+                            : Text(
+                                user.value.aboutMe!,
+                                softWrap: true,
+                              ),
+                      ),
                     ],
                   ),
                 ],
