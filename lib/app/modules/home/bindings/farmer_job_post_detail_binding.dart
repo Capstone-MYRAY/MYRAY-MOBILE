9d3574a386a34f5e4da1032b254f9ae06422dbaa
Nguyen Phan Quynh Anh <anhnpqse140645@gmail.com>
2022-07-18 18:03:33 +0700

    book mark and unbook mark in farmer detail

--- lib/app/modules/home/bindings/farmer_job_post_detail_binding.dart
+++ lib/app/modules/home/bindings/farmer_job_post_detail_binding.dart
@@ -1,6 +1,7 @@
 
 import 'package:get/get.dart';
 import 'package:myray_mobile/app/data/models/job_post/job_post.dart';
+import 'package:myray_mobile/app/modules/bookmark/bookmark_repository.dart';
 import 'package:myray_mobile/app/modules/home/controllers/farmer_job_post_detail_controller.dart';
 import 'package:myray_mobile/app/shared/constants/constants.dart';
 
@@ -10,6 +11,7 @@ class FarmerJobPostDetailBinding extends Bindings{
     JobPost jobPost = Get.arguments[Arguments.item];
 
     Get.lazyPut(() => FarmerJobPostDetailController(jobPost: jobPost));
+    Get.lazyPut(() => BookmarkRepository(), fenix: true);
   }
 
 }
