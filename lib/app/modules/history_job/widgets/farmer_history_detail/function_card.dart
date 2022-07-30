d340feb86170c362bc976cf53c674a7739b51bca
Nguyen Phan Quynh Anh <anhnpqse140645@gmail.com>
2022-07-30 09:06:13 +0700

    UI history detail

---
+++ lib/app/modules/history_job/widgets/farmer_history_detail/function_card.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:myray_mobile/app/shared/constants/app_colors.dart';
+import 'package:myray_mobile/app/shared/icons/custom_icons_icons.dart';
+
+class FunctionCard extends StatelessWidget {
+  final void Function()? feedback;
+  final void Function()? report;
+  final void Function()? bookmark;
+  final bool? isBookmark;
+
+  const FunctionCard({
+    Key? key,
+    this.isBookmark = false,
+    this.feedback,
+    this.report,
+    this.bookmark
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(9),
+          ),
+          color: AppColors.white,
+          child: Container(
+            width: Get.width * 0.32,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 5.0),
+              child: Column(children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    const Icon(
+                      Icons.feedback_outlined,
+                      size: 25,
+                      color: AppColors.primaryColor,
+                    ),
+                    TextButton(
+                      onPressed: feedback,
+                      child: Text(
+                        'Đánh giá',
+                        style: Get.textTheme.headline6!.copyWith(
+                          color: AppColors.primaryColor,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    const Icon(
+                      Icons.flag_outlined,
+                      size: 25,
+                      color: AppColors.primaryColor,
+                    ),
+                    TextButton(
+                      onPressed: report,
+                      child: Text(
+                        'Báo cáo',
+                        style: Get.textTheme.headline6!.copyWith(
+                          color: AppColors.primaryColor,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    isBookmark!
+                    ? const Icon(
+                      CustomIcons.heart,
+                      size: 25,
+                      color: Colors.red,
+                    )
+                    : const Icon(
+                      CustomIcons.heart_outline,
+                      size: 25,
+                      color: Colors.red,
+                    ),
+                    TextButton(
+                      onPressed: bookmark,
+                      child: Text(
+                        'Yêu thích',
+                        style: Get.textTheme.headline6!.copyWith(
+                          color: AppColors.primaryColor,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ]),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+//Underline Text Button
+
+// TextButton(
+//                 onPressed: () {},
+//                 child: Text(
+//                   'Báo cáo',
+//                   style: Get.textTheme.headline5!.copyWith(
+//                       color: AppColors.primaryColor,
+//                       shadows: const [
+//                         Shadow(
+//                             color: AppColors.primaryColor,
+//                             offset: Offset(0, -5))
+//                       ],
+//                       decoration: TextDecoration.underline,
+//                       decorationColor: AppColors.primaryColor,
+//                       decorationThickness: 1),
+//                 ),
+//               ),
---
+++ lib/app/modules/history_job/widgets/farmer_history_detail/landowner_cart.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:myray_mobile/app/modules/history_job/widgets/farmer_history_detail/function_card.dart';
+import 'package:myray_mobile/app/shared/constants/app_colors.dart';
+import 'package:myray_mobile/app/shared/icons/custom_icons_icons.dart';
+
+class LandOwnerCard extends StatelessWidget {
+  final void Function()? feedback;
+  final void Function()? report;
+  final void Function()? bookmark;
+  final bool? isBookmark;
+  final String name;
+  final String address;
+  const LandOwnerCard(
+      {Key? key,
+      required this.name,
+      required this.address,
+      this.isBookmark = false,
+      this.feedback,
+      this.report,
+      this.bookmark})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(9),
+      ),
+      color: AppColors.white,
+      child: Container(
+        width: Get.width * 0.90,
+        padding: const EdgeInsets.all(10),
+        // width: Get.width * 0.55,
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Icon(
+                        CustomIcons.person,
+                        size: 20,
+                      ),
+                      const SizedBox(
+                        width: 15,
+                      ),
+                      SizedBox(
+                        width: 130,
+                        child: Text(
+                          name,
+                          style: Get.textTheme.bodyText2!.copyWith(
+                            fontSize: Get.textScaleFactor * 15,
+                          ),
+                          softWrap: true,
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: Get.height * 0.02,
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Icon(
+                        CustomIcons.map_marker_outline,
+                        size: 20,
+                      ),
+                      const SizedBox(
+                        width: 15,
+                      ),
+                      SizedBox(
+                        width: 120,
+                        child: Text(
+                          address,
+                          style: Get.textTheme.bodyText2!.copyWith(
+                            fontSize: Get.textScaleFactor * 15,
+                          ),
+                          softWrap: true,
+                          maxLines: 5,
+                          overflow: TextOverflow.ellipsis,
+                          textAlign: TextAlign.left,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            FunctionCard(
+              feedback: feedback,
+              report: report,
+              bookmark: bookmark,
+              isBookmark: true,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
