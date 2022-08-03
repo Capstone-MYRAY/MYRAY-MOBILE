f22e0b70e9e3ca50491683b2f9d80931794f6e97
Nguyen Phan Quynh Anh <anhnpqse140645@gmail.com>
2022-08-03 08:04:44 +0700

    fix onLeave list

--- lib/app/modules/feedback/controllers/feedback_controller.dart
+++ lib/app/modules/feedback/controllers/feedback_controller.dart
@@ -1,4 +1,3 @@
-
 import 'package:get/get.dart';
 import 'package:myray_mobile/app/data/models/feedback/feedback.dart';
 import 'package:myray_mobile/app/data/models/feedback/get_feedback_request.dart';
@@ -7,15 +6,22 @@ import 'package:myray_mobile/app/data/models/feedback/post_feedback_request.dart
 import 'package:myray_mobile/app/data/models/feedback/put_feedback_request.dart';
 import 'package:myray_mobile/app/modules/feedback/feedback_repository.dart';
 
-class FeedBackController extends GetxController{
-
+class FeedBackController extends GetxController {
   final FeedBackRepository _feedBackRepository = Get.find<FeedBackRepository>();
 
-  Future<FeedBack?> sendFeedBack(PostFeedbackRequest data) async {    
+  List<String> recommendFeedbackContent = [
+    'Chủ tốt và thân thiện',
+    'Chủ rất uy tín',
+    'Bao ăn ở rất chu đáo',
+    'Rất quan tâm đến nhu cầu của nông dân',
+    'Khác'
+  ];
+
+  Future<FeedBack?> sendFeedBack(PostFeedbackRequest data) async {
     return await _feedBackRepository.sendFeedBack(data);
   }
 
-   Future<FeedBack?> updateFeedback(PutFeedbackRequest data) async {
+  Future<FeedBack?> updateFeedback(PutFeedbackRequest data) async {
     return await _feedBackRepository.updateFeedback(data);
   }
 
